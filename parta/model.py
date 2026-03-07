fa4b91a3a252d26bd3de1524d123449a7c51ca48
KoRaghav <koraghav@outlook.com>
2026-03-07 13:10:39 +0530

    Create transformer and attention networks

--- parta/model.py
+++ parta/model.py
@@ -2,7 +2,89 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from typing import Any, Dict, List
+from dataclass import dataclass
 
+@dataclass
+class AttentionWeights:
+    W_Q: torch.Tensor
+    W_K: torch.Tensor
+    W_V: torch.Tensor
+
+class Attention(nn.Module):
+    def __init__(self, d_model: int, mode: str):
+        self.d_model = d_model
+        self.W_Q: torch.Tensor
+        self.W_K: torch.Tensor
+        self.W_V: torch.Tensor
+    
+    def set_weights(self, weights: AttentionWeights):
+        self.W_Q = weights.W_Q
+        self.W_K = weights.W_K
+        self.W_V = weights.W_V
+
+    def forward(self, input: torch.Tensor):
+        Q = input * self.W_Q
+        K = input * self.W_K
+        V = input * self.W_V
+        S = Q * K.t() / torch.sqrt(self.d_model)
+        Attn = F.softmax(S, axis=-1)
+        return Attn * V
+
+@dataclass
+class TransformerBlockWeights:
+    attention_weights: list[AttentionWeights]
+    beta_1: torch.Tensor
+    beta_2: torch.Tensor
+    gamma_1: torch.Tensor
+    gamma_2: torch.Tensor
+    W_O: torch.Tensor
+    W_up: torch.Tensor
+    W_down: torch.Tensor
+    b_up: torch.Tensor
+    b_down: torch.Tensor
+
+class TransformerBlock(nn.Module):
+    def __init__(self, d_model: int, mode: str, n_heads: int):
+        super().__init__()
+        self.d_model = d_model
+        self.n_heads = n_heads
+
+        self.layernorm_1 = nn.LayerNorm(self.d_model)
+        self.layernorm_2 = nn.LayerNorm(self.d_model)
+
+        self.heads = [Attention(d_model, mode) for _ in range(n_heads)]
+        self.W_O: torch.Tensor
+
+        self.W_up: torch.Tensor
+        self.b_up: torch.Tensor
+        self.W_down: torch.Tensor
+        self.b_down: torch.Tensor
+
+    def set_weights(self, weights: TransformerBlockWeights):
+        self.layernorm_1.weight = weights.gamma_1
+        self.layernorm_1.bias = weights.beta_1
+        self.layernorm_2.weight = weights.gamma_2
+        self.layernorm_2.bias = weights.beta_2
+        
+        for i in range(self.n_heads):
+            self.heads[i].set_weights(weights.attention_weights[i])
+        self.W_O = weights.W_O
+
+        self.W_up = weights.W_up
+        self.b_up = weights.b_up
+        self.W_down = weights.W_down
+        self.b_down = weights.b_down
+
+    def forward(self, x):
+        u = self.layernorm_1(x)
+        attn = torch.cat(att(u) for att in self.heads) * self.W_O
+        z1 = x + attn
+        v1 = self.layernorm_2(z1)
+        v2 = v1 * self.W_up + self.b_up
+        v3 = F.gelu(v2)
+        v4 = v3 * self.W_down + self.b_down
+        z2 = z1 + v4
+        return z2
 
 class LanguageModel(nn.Module):
     """
@@ -23,7 +105,12 @@ class LanguageModel(nn.Module):
         self.MODE = config["mode"]
         self.TAU = config.get("tau")
 
-        self.weights = None
+        self.W_vocab: torch.Tensor
+        self.W_devocab: torch.Tensor
+        self.transformer_blocks = [TransformerBlock(self.D_MODEL, self.MODE, self.N_HEADS) for _ in range(self.N_LAYERS)]
+        self.beta_final: torch.Tensor
+        self.gamma_final: torch.Tensor
+
         super().__init__()
 
     def set_weights(self, weights: Dict[str, Any]):
@@ -35,7 +122,28 @@ class LanguageModel(nn.Module):
         Parameters:
             - weights: A dictionary containing the model's weights. The structure of this dictionary will depend on how you design your model.
         """
-        self.weights = weights
+        self.W_vocab = weights["W_vocab"]
+        self.W_devocab = weights["W_devocab"]
+        self.beta_final = weights["beta_final"]
+        self.gamma_final = weights["gamma_final"]
+
+        for l in range(self.N_LAYERS):
+            attention_weights = []
+            for k in range(self.N_HEADS):
+                attention_weights.append(AttentionWeights(weights[f"W_{l}_Q_{k}"], weights[f"W_{l}_K_{k}"], weights[f"W_{l}_V_{k}"]))
+            transformer_weights = TransformerBlockWeights(
+                attention_weights,
+                weights[f"beta_{l}_1"],
+                weights[f"beta_{l}_2"],
+                weights[f"gamma_{l}_1"],
+                weights[f"gamma_{l}_2"],
+                weights[f"W_{l}_O"],
+                weights[f"W_{l}_up"],
+                weights[f"W_{l}_down"],
+                weights[f"b_{l}_up"],
+                weights[f"b_{l}_down"],
+            )
+            self.transformer_blocks[l].set_weights(transformer_weights)
         
 
     def forward(self, input_ids: torch.Tensor, attention_mask: torch.Tensor) -> torch.Tensor:
@@ -52,8 +160,16 @@ class LanguageModel(nn.Module):
         """
         batch_size, l = input_ids.shape
 
-        embed_layer = nn.Embedding(self.VOCAB_SIZE, self.D_MODEL, _weight=torch.transpose(self.weights["W_vocab"], 0, 1))
+        embed_layer = nn.Embedding(self.VOCAB_SIZE, self.D_MODEL, _weight=torch.transpose(self.W_vocab, 0, 1))
         embedded = embed_layer(input_ids)
+
+        pos_embed_even = torch.sin(torch.arange(l).unsqueeze(1) / (10000 ** (torch.arange(0, self.D_MODEL, 2).unsqueeze(0).expand(l, -1) / self.D_MODEL)))
+        pos_embed_odd = torch.cos(torch.arange(l).unsqueeze(1) / (10000 ** (torch.arange(1, self.D_MODEL, 2).unsqueeze(0).expand(l, -1) / self.D_MODEL)))
+        pos_embed = torch.zeros(l, self.D_MODEL)
+        pos_embed[:, 0::2] = pos_embed_even
+        pos_embed[:, 1::2] = pos_embed_odd
+
+        x_0 = embedded + pos_embed.unsqueeze(0)
 
         raise NotImplementedError("Implement forward as described in assignment document")
 
